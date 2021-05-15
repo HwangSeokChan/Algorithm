55c83f97d6f3371aebd66dbb1df60f318cf33b14
on-Sync <putstack.career@gmail.com>
2021-05-15 20:38:19 +0900

    튜플 / 다른 풀이 추가

--- Programmers/2단계/튜플/src/Solution.java
+++ Programmers/2단계/튜플/src/Solution.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.util.stream.Stream;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Solution{
     public int[] solution(String s) {
@@ -39,4 +41,24 @@ class SolutionB {
         return counts.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .map(Map.Entry::getKey).mapToInt(x -> x).toArray();
     }
+}
+
+class SolutionC {
+    public int[] solution(String s) {
+
+        Map<String, Integer> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            String n = matcher.group();
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        int size = map.size();
+        int[] answer = new int[size];
+        for (String key: map.keySet()) {
+            answer[size - map.get(key)] = Integer.parseInt(key);
+        }
+        return answer;
+    }
+
 }
