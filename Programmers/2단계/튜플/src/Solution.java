d87bc6ad0adbf2c1176c7f69d45d0d77520d1a10
on-Sync <putstack.career@gmail.com>
2021-05-15 20:30:57 +0900

    튜플 / 성능비교, 다른 정답자 풀이

--- Programmers/2단계/튜플/src/Solution.java
+++ Programmers/2단계/튜플/src/Solution.java
@@ -1,11 +1,10 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.ArrayList;
-
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Solution{
     public int[] solution(String s) {
         String[] arr = s.substring(2, s.length()-2).split("\\}\\,\\{");
+        
         Arrays.sort(arr, new Comparator<String>(){
             @Override
             public int compare(String o1, String o2) {
@@ -13,20 +12,31 @@ public class Solution{
             }
         });
 
-        ArrayList<String> list = new ArrayList<String>();
+        HashSet<String> set = new HashSet<String>();
+        int[] answer = new int[arr.length];
+        int idx = 0;
         for (String sub : arr) {
             for (String item : sub.split(",")) {
-                if (!list.contains(item)) {
-                    list.add(item);
+                if (!set.contains(item)) {
+                    if (set.add(item))
+                        answer[idx++] = Integer.parseInt(item);
                 }
             }
         }
 
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = Integer.parseInt(list.get(i));
-        }
-
         return answer;
+    }
+}
+
+class SolutionB {
+    final Map<Integer, Integer> counts = new LinkedHashMap<>();
+
+    public int[] solution(String s) {
+        
+        Stream.of(s.replaceAll("[}{]", "").split(",")).mapToInt(Integer::parseInt)
+                .forEach(i -> counts.merge(i, 1, Integer::sum));
+
+        return counts.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .map(Map.Entry::getKey).mapToInt(x -> x).toArray();
     }
 }
