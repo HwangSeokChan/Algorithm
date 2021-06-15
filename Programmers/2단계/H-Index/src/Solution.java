ba400b51847c8e4cc0f1cf2a63575ee84cdc1341
on-Sync <putstack.career@gmail.com>
2021-06-15 19:54:54 +0900

    H-Index / 정의대로 적용한 풀이 추가

--- Programmers/2단계/H-Index/src/Solution.java
+++ Programmers/2단계/H-Index/src/Solution.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
-    public int solution(int[] citations) {
+    public int solutionA(int[] citations) {
         for (int i = citations.length; i > 0; i--) {
             int above = 0;
             int below = 0;
@@ -20,5 +22,23 @@ class Solution {
         }
 
         return 0;
+    }
+
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+
+        int h_index = 0;
+        for(int i = citations.length-1; i >= 0; i--){
+            int no = citations.length - i;
+            int citation = citations[i];
+            int score = Math.min(citation, no);
+            if(h_index < score) 
+                h_index = score;
+            else 
+                break;
+        }
+
+        return h_index;
     }
 }
